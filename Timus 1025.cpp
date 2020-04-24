25de81063f542d1e7e23781387235e6f3f06657b
MahtabTanim <63159321+MahtabTanim@users.noreply.github.com>
2020-04-24 19:59:07 +0600

    Add files via upload

---
+++ Timus 1025.cpp
@@ -0,0 +1,31 @@
+// Written By Mahtab Tanim
+#include<bits/stdc++.h>
+using namespace std;
+#define MP ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define nl "\n"
+#define End return 0
+#define ll long long int
+#define mp make_pair
+#define pb push_back
+#define ull unsigned long long int
+#define pi acos(-1)
+#define forn(i,a,b) for (int i=a;i<=b;i++)
+
+int main()
+{
+    int n;
+    cin>>n;
+    int x[n],j;
+    for(int i=0;i<n;i++)
+    {
+        cin>>x[i];
+    }
+    sort(x,x+n);
+    int sum=0;
+    for(int i=0;i<(n+1)/2;i++)
+    {
+        sum+=(x[i]+1)/2;
+    }
+    cout<<sum<<endl;
+    return 0;
+}
